8a7304d5da8b71fcd746bd63305a5907a0f7d715
Marcus Beladona <mv3265@gmail.com>
2019-06-01 19:34:43 -0300

    Inicial

---
+++ Interface/App.java
@@ -0,0 +1,14 @@
+package Interface;
+
+import Util.*;
+
+public class App extends Helper {
+
+    public static void main(String[] args) {
+
+        int a = nextInt();
+        int b = nextInt();
+
+        printf("Seus números são %d e %d.", a, b);
+    }
+}
---
+++ Util/Helper.java
@@ -0,0 +1,55 @@
+package Util;
+
+import java.util.Scanner;
+
+// Eu sou Bob o ajudante!
+// Fui feito para simplificar seu código.
+// Extenda suas classes com esta para que eu possa facilitar o seu trabalho.
+
+public class Helper {
+
+    // Printer:
+
+    // Estas funções sempre irão dar um \n no final.
+    // Você pode mudar isso se quiser.
+
+    public static void print(Object aux) {
+        System.out.println(aux);
+    }
+
+    public static void print() {
+        System.out.println();
+    }
+
+    public static void printf(String format, Object... args) {
+        System.out.printf(format, args);
+        print();
+    }
+
+    // Scanner:
+
+    // Não use 'scan.close'.
+    // Você pode incluir novas funções conforme a necessidade.
+
+    static Scanner scan = new Scanner(System.in);
+
+    public static int nextInt() {
+        return scan.nextInt();
+    }
+
+    public static double nextDouble() {
+        return scan.nextDouble();
+    }
+
+    public static String nextLine() {
+        return scan.nextLine();
+    }
+
+    public static float nextFloat() {
+        return scan.nextFloat();
+    }
+
+    public static short nextShort() {
+        return scan.nextShort();
+    }
+}
